d76230dac8317d199cd90a61d62d3d3f6b95aac7
Ramya Challa <ramyac6@cs.washington.edu>
2022-04-25 13:17:38 -0700

    more cleanup of comments

--- scripts/get_time_data.py
+++ scripts/get_time_data.py
@@ -1,12 +1,8 @@
 import rosbag, sys, csv
 import time
-import string
 import os #for file management make directory
-import shutil #for file management, copy file
 import rosbag
-import numpy as np
 
-# For now, manually specify which topic contains the image messages.
 
 #verify correct input arguments: 1 or 2
 if (len(sys.argv) == 1):
@@ -41,17 +37,10 @@ with open("information.csv", 'w+') as csvfile:
             if topic not in listOfTopics:
                 listOfTopics.append(topic)
 
-        # pull from the forquebody topic
-            # find the first five seconds of continuous tracking
-                # determine how frequently messages are sent
-                # calculate the number of messages that should be sent in five seconds
-                # use sequence numbers and timestamps to figure out how many messages have been sent in a time period
-            # beginning of those five seconds is our start time
         
         for topicName in listOfTopics:
             if topicName == '/vrpn_client_node/ForqueBody/pose':
                 print("Extracting topic: " + topicName)
-                #Create a new CSV file for each topic
                     
                 firstIteration = True	#allows header row
                 current = None
@@ -60,8 +49,7 @@ with open("information.csv", 'w+') as csvfile:
                 current
                 start_time = 0 # value should never be zero if reading a valid bag file
                 for subtopic, msg, t in bag.read_messages(topicName):	# for each instant in time that has data for topicName
-                    #parse data from this instant, which is of the form of multiple lines of "Name: value\n"
-                    #	- put it in the form of a list of 2-element lists
+
                     current = t
                     if previous == None:  # first real value
                         start_time = current.to_sec()
@@ -71,7 +59,7 @@ with open("information.csv", 'w+') as csvfile:
                         if difference >= 0.1:
                             time_lost += difference
                         previous = current
-                            
+                # todo maybe: change name of bag file to just the participant, trial, and food            
                 filewriter.writerow([bagName,start_time,time_lost,current.to_sec(),current.to_sec()-start_time])                
         bag.close()
 print("Done reading all " + numberOfFiles + " bag files.")
